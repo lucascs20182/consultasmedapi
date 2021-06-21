35cf7103db5a953776f57f2ffd5a251f98478780
Lucas Cruz <lucascs20182@gmail.com>
2021-06-21 17:40:11 -0300

    configure cors e libere os headers para authorization

--- src/main/java/br/edu/unifeso/apiconsultasmed/config/SecurityConfig.java
+++ src/main/java/br/edu/unifeso/apiconsultasmed/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.edu.unifeso.apiconsultasmed.security.AuthService;
 import br.edu.unifeso.apiconsultasmed.security.JWTAutheticationFilter;
