45924966063c7a7f3b9df0db1a2621f8830f4fe6
Lucas Cruz <lucascs20182@gmail.com>
2021-06-21 17:54:19 -0300

    configure cors e libere os headers para authorization

--- src/main/java/br/edu/unifeso/apiconsultasmed/config/SecurityConfig.java
+++ src/main/java/br/edu/unifeso/apiconsultasmed/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -32,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
 		http.csrf().disable();
 		http.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated();
 		http.addFilterBefore(new JWTAutheticationFilter(authenticationManager(), jwtUtil),
